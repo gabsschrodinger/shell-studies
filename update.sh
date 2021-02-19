0e33c46610aa5efcf520e4755039e39948bfee6c
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-02-19 08:18:35 -0300

    Updating update script

--- update.sh
+++ update.sh
@@ -8,15 +8,15 @@ echo "💊 Adding all files to staging area... 💊"
 git add .
 echo "✔ Done!"
 echo --------------------------------------------------
-echo
-echo "💬 Insert yout commit message:"
+echo "💬 Insert your commit message:"
 read COMMIT_MESSAGE
-echo "✔ commiting changes"
+echo "🚀 commiting changes..."
 git commit -m "${COMMIT_MESSAGE}"
-echo "Done with this message: "\"${COMMIT_MESSAGE}\"""
+echo "✔ Done with this message: "\"${COMMIT_MESSAGE}\""."
 echo --------------------------------------------------
-echo "💬 Please, insert the origin branch"
+echo "💬 Insert the origin branch:"
 read BRANCH_NAME
-echo "🎉 pushing changes to $BRANCH_NAME branch"
+echo "🎉 Pushing changes to ${BRANCH_NAME} branch..."
 git push origin $BRANCH_NAME
+echo "✔ Done!"
 echo --------------------------------------------------
